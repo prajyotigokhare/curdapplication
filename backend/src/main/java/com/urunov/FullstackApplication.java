588f6c9e0c567754a1d8620488eaadb53d40e27c
Omkar Phadtare <omkarphadtare321@gmail.com>
2023-06-28 13:56:08 +0530

    update

--- backend/src/main/java/com/urunov/FullstackApplication.java
+++ backend/src/main/java/com/urunov/FullstackApplication.java
@@ -1,17 +1,26 @@
 package com.urunov;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@SpringBootConfiguration
 @SpringBootApplication
 //@EnableSwagger2
 @CrossOrigin
-public class FullstackApplication {
+public class FullstackApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(FullstackApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // TODO Auto-generated method stub
+        return builder.sources(FullstackApplication.class);
     }
 
 }
